21f5997439fd57f4b05e2e71f66abdc04f143669
Udbhav <ukush4@gmail.com>
2017-03-23 22:56:29 +0530

    Listview for cycles created

--- app/src/main/java/com/example/udbhav/cyclerental/MainActivity.java
+++ app/src/main/java/com/example/udbhav/cyclerental/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.udbhav.cyclerental;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity{
+
+    ListView list;
+    String[] itemname ={
+            "Cycle1",
+            "Cycle2",
+            "Cycle1",
+            "Cycle2",
+            "Cycle1",
+            "Cycle2",
+            "Cycle1",
+            "Cycle2"
+    };
+
+    Integer[] imgid={
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p1,
+            R.drawable.p2,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomAdapter adapter=new CustomAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        
     }
 }
